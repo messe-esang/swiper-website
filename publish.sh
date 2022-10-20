e2277c8604116f010de40c3cfd0391b5f1c037e6
Build <exit.es@esgroup.net>
2022-10-20 13:36:48 +0900

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -18,6 +18,8 @@ aws s3 sync . s3://esang-mim/docs/swiper-v5     \
     --exclude="package.json"  \
     --exclude="README.md"  \
     --exclude="yarn.lock"  \
+    --exclude=".git"  \
+    --exclude="publish.sh"  \
     --acl public-read
 
 # 
